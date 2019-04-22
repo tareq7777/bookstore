359d63f74c707ca57274cba75c58013cb4bb3e4f
akram <akram@a.a>
2019-04-22 15:34:42 +0300

    Apis fixes

--- api/models/book.js
+++ api/models/book.js
@@ -15,4 +15,10 @@ Book.getBooksById = (bookId, response) => {
     });
 };
 
+Book.addNewBook = (book, response) => {
+    db.query("INSERT INTO books VALUES (?,?,?,?)", [book.title, book.isbn, book.author, book.keywords, book.publisher], (err, res) => {
+        err ? functions.response(response, -99) : functions.response(response, 1, res);
+    });
+};
+
 module.exports = Book;
