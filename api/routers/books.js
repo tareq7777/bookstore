1e51ace17369d1a910d96d4e718aa52fa86eedc2
adnanki208 <adnanki208@github.com>
2019-04-22 15:36:09 +0300

    add book

--- api/routers/books.js
+++ api/routers/books.js
@@ -14,49 +14,23 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    
     let title = req.body.title;
     let isbn = parseInt(req.body.isbn);
     let author = req.body.author;
     let keywords = req.body.keywords;
     let publisher = req.body.publisher;
-    if( title  && isbn && author && keywords && publisher){
-    const book = {
-        id: 0, title: title, isbn: isbn, keywords: keywords
-        , publisher: publisher , author: author
-    };
-    
-    User.createUser(user, (err, result) => {
-        if (err) {
-            res.status(500).json({error: err});
-        } else {
-            result = user;
 
-            res.status(201).json({message: 'User created successfully', createdUser: result});
-
-        }
-    });
-    }else{
+    if (title && isbn && author && keywords && publisher) {
+        const book = {
+            title: title, isbn: isbn, keywords: keywords
+            , publisher: publisher, author: author
+        };
+        Book.addNewBook(book, res);
+    } else {
         functions.response(res, -10);
     }
 
-})
-// var courses = new Array;
-//     courses=[
-//         {id: 1,name: "php"},
-//         {id: 2,name: "angular"},
-//         {id: 3,name: "js"},
-//         {id: 4,name: "css"},
-//         {id: 5,name: "html"},
-//     ]
-// app.use(express.json());
+});
 
-// router.get('/',(req, res) => {
-//     res.status(200).send(courses);
-// });
-// router.get('/:id',(req, res) => {
-//     res.status(200).send("get with id:"+parseInt(req.params.id));
-// });
-// router.post('/',(req, res) => {
-
-// });
 module.exports = router;
